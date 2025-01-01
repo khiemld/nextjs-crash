c166035918c903b8f6cd4f112364015b523f52ee
khiemld <khiemld.0204@gmail.com>
2025-01-01 21:29:46 +0700

    feat: Add header section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,15 @@
 export default function Home() {
   console.log("Hello World! Run from the server side");
   return (
-    <div>
-      <div className="text-2xl">HOME</div>
+    <div className="w-full">
+      <section className="pink_container">
+        <h1 className="heading ">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs{" "}
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.
+        </p>
+      </section>
     </div>
   );
 }
