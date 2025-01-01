03e01a009052ab5428269ae7c4a71b93aa06f7f9
khiemld <khiemld.0204@gmail.com>
2025-01-02 00:24:33 +0700

    feat: Add search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
-  console.log("Hello World! Run from the server side");
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
-    <div className="w-full">
+    <div>
       <section className="pink_container">
         <h1 className="heading ">
           Pitch Your Startup, <br /> Connect With Entrepreneurs{" "}
@@ -9,6 +15,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.
         </p>
+        <SearchForm query={query} />
       </section>
     </div>
   );
